743377de63016f67080443500ab8e98c3657db01
ThirdyNeko <ireneo.catequista@wvsu.edu.ph>
2026-02-12 11:44:18 +0800

    feat: implement admin dashboard and user creation functionality

--- auth/login.php
+++ auth/login.php
@@ -40,7 +40,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 break;
     
             case 'admin':
-                header('Location: ../auth/create_user.php');
+                header('Location: ../admin/admin.php');
                 break;
 
             default:

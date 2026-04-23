42c3627d2ac4a34901ea86c34253018af3b9f037
ThirdyNeko <ireneo.catequista@wvsu.edu.ph>
2026-04-23 08:03:26 +0800

    refactor(branch): update branch mapping from SHOWROOM to VIAC

--- hooks/branch.php
+++ hooks/branch.php
@@ -3,7 +3,7 @@
 function getBranchByIp(string $ip): ?string
 {
     static $map = [
-        'SHOWROOM' => [
+        'VIAC' => [
             '192.168.40.0/24',
             '::1' // localhost IPv6
         ]
